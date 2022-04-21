fd9d7c81bb57effa9ac4b309daeb449dff38c23d
Dawid Sygocki <d.sygocki@stud.elka.pw.edu.pl>
2022-04-21 21:14:27 +0200

    Set up C# project with testing, etc.

---
+++ Toffee/Program.cs
@@ -0,0 +1,18 @@
+﻿using Toffee;
+
+if (args.Length > 1)
+{
+    Console.WriteLine("Usage: Toffee [script]");
+    return 64;
+}
+else if (args.Length == 1)
+{
+    using var reader = new StreamReader(args[0]);
+    var runner = new Runner(reader, args[0]);
+    return runner.Run();
+}
+else
+{
+    var runner = new Runner(Console.In, "STDIN");
+    return runner.Run();
+}
---
+++ Toffee/Runner.cs
@@ -0,0 +1,18 @@
+﻿namespace Toffee;
+
+public class Runner
+{
+    private readonly TextReader _reader;
+    private readonly string _sourceName;
+    
+    public Runner(TextReader reader, string sourceName)
+    {
+        _reader = reader;
+        _sourceName = sourceName;
+    }
+
+    public int Run()
+    {
+        return 0;
+    }
+}
