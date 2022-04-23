f0ec944e0c6e092d4dde58b1bc48e0394fd865d8
Dawid Sygocki <d.sygocki@stud.elka.pw.edu.pl>
2022-04-23 21:48:31 +0200

    Implement escape sequences in string matcher

--- Toffee/LexicalAnalysis/Lexer.cs
+++ Toffee/LexicalAnalysis/Lexer.cs
@@ -199,16 +199,69 @@ public class Lexer : ILexer
 
     private Token? MatchString()
     {
-        // TODO: escaping
+        static bool IsHexDigit(char? c) => c is >= '0' and <= '9' or >= 'A' and <= 'F' or >= 'a' and <= 'f';
+        static long CharToDigit(char c) => c >= 'a'
+            ? c - 'a' + 10
+            : c >= 'A'
+                ? c - 'A' + 10
+                : c - '0';
+
         // TODO: limit length
         if (_scanner.CurrentCharacter is not '"')
             return null;
         _scanner.Advance();
         var contentBuilder = new StringBuilder();
-        while (_scanner.CurrentCharacter is not (null or '"'))
+        var escaping = false;
+        while (_scanner.CurrentCharacter is not null)
         {
-            contentBuilder.Append(_scanner.CurrentCharacter.Value);
-            _scanner.Advance();
+            if (escaping)
+            {
+                escaping = false;
+                if (_scanner.CurrentCharacter is 'x')
+                {
+                    // TODO: support 2-byte chars
+                    // TODO: use BitConverter
+                    _scanner.Advance();
+                    if (!IsHexDigit(_scanner.CurrentCharacter))
+                        new string("Missing hex byte specification");  // TODO: error
+                    var escapedByte = (byte)CharToDigit(_scanner.CurrentCharacter.Value);
+                    _scanner.Advance();
+                    if (IsHexDigit(_scanner.CurrentCharacter))
+                    {
+                        escapedByte = (byte)(16 * escapedByte + CharToDigit(_scanner.CurrentCharacter.Value));
+                        _scanner.Advance();
+                    }
+                    contentBuilder.Append((char)escapedByte);
+                }
+                else
+                {
+                    contentBuilder.Append(_scanner.CurrentCharacter switch
+                    {
+                        'a' => '\a',
+                        'b' => '\b',
+                        'f' => '\f',
+                        'n' => '\n',
+                        'r' => '\r',
+                        't' => '\t',
+                        'v' => '\v',
+                        '\\' => '\\',
+                        '"' => '"',
+                        '0' => '\0',
+                        _ => _scanner.CurrentCharacter.Value // TODO: error
+                    });
+                    _scanner.Advance();
+                }
+            }
+            else
+            {
+                if (_scanner.CurrentCharacter is '"')
+                    break;
+                if (_scanner.CurrentCharacter is '\\')
+                    escaping = true;
+                else
+                    contentBuilder.Append(_scanner.CurrentCharacter.Value);
+                _scanner.Advance();
+            }
         }
         if (_scanner.CurrentCharacter is '"')
             _scanner.Advance();
